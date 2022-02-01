ce07ef785969e574379e9e26dd174426da956d6a
Tosmel2 <Adewaletosin0808@gmail.com>
2022-02-01 13:39:11 -0800

    Python script to export data in the JSON format.

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''
+export to JSON
+'''
+
+import json
+import requests
+from sys import argv
+
+
+def getData(id):
+    '''get data  adn export of employee'''
+    URL = 'https://jsonplaceholder.typicode.com/'
+    ENDPOINT = URL + 'users/{}'.format(id)
+    employee = requests.get(ENDPOINT).json()
+    TASKENDPOINT = URL + 'todos?userId={}'.format(employee['id'])
+    tasks = requests.get(TASKENDPOINT).json()
+    data = {"employee": employee, "tasks": tasks}
+    _id = data['employee']['id']
+    _allData = []
+    for task in data['tasks']:
+        employee = {}
+        employee['task'] = task['title']
+        employee['completed'] = task['completed']
+        employee['username'] = data['employee']['username']
+        _allData.append(employee)
+    _final = {_id: _allData}
+    with open('{}.json'.format(_id), 'w') as f:
+        json.dump(_final, f)
+
